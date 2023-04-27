e9c7a961034feebf50865f2e19ce5440c321e789
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-04-26 20:00:06 -0500

    init

--- server.js
+++ server.js
@@ -1,122 +1,49 @@
-
-///variables///////////
-
-// Import express package
+// Import required packages and define variables
 const express = require('express');
-// Initialize our app variable by setting it to the value of express()
-const app = express();
-//port will be 3000
-var PORT = process.env.PORT || 3000;
-//will be using absolute path to public directory at some point
 const path = require('path');
-// Require the JSON file and assign it to a variable called `noteData`
-const noteData = require('/db.json');
+const fs = require('fs');
 
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+// Read the JSON file and assign it to a variable called `noteData`
+const noteData = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
 
+// Initialize app and set port
+const app = express();
+var PORT = process.env.PORT || 3000;
 
-
-////////////setting up middleware //////////
-// Serve static files from the 'public' directory
+// Middleware
 app.use(express.static(path.join(__dirname, 'public')));
-//parse incoming request bodies that contain URL-encoded data.
 app.use(express.urlencoded({ extended: true }));
-//parse incoming request bodies that contain JSON data
 app.use(express.json());
-//middleware for routes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
-
-
-
-
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-
-
-
-
-app.get('/notes', (req, res) => {
-    res.send('This is an example GET request');
-  });
-
-  app.post('/example', (req, res) => {
-    res.send('This is an example POST request');
-  });
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  });
-
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
-);
-
-
-// GET route that returns any specific term
-app.get('/api/term/:term', (req, res) => {
-  const requestedTerm = req.params.term.toLowerCase();
-    // http://localhost:3001/api/term/seo
-    console.log("req.params", req.params);
-    // http://localhost:3001/api/term/seo?t=moana
-    console.log("req.query", req.query); 
-
-  // Iterate through the terms name to check if it matches `req.params.term`
-  if (requestedTerm) {
-    for (let i = 0; i < termData.length; i++) {
-      if (requestedTerm === termData[i].term.toLowerCase()) {
-        return res.json(termData[i]);
-      }
-    }
-  }
-
-  // Return a message if the term doesn't exist in our DB
-  return res.json('No term found');
-});
-
-// GET route for returning all terms from a given category
-app.get('/api/terms/:category', (req, res) => {
-  const requestedCategory = req.params.category.toLowerCase();
-  const result = [];
-
-  for (let i = 0; i < termData.length; i++) {
-    const currentTermCategory = termData[i].category;
-    if (requestedCategory === currentTermCategory) {
-      result.push(termData[i]);
-    }
-  }
-  return res.json(result);
-});
-
-
 
 // Routes
-// http://localhost:3000/?t=The%20Goonies&apikey=farley
-// { t: 'mean girls', apikey: 'farley' }
 app.get('/', (req, res) => {
-	console.log("req.query: ", req.query)
-	if (req.query.t && req.query.apikey){
-		const searchTitle = req.query.t.toLowerCase()
-		console.log("searchTitle: ", searchTitle)
-	
-
-		for (let i = 0; i < movieData.length; i++) {
-			if (movieData[i].Title.toLowerCase() === searchTitle) {
-				return res.json(movieData[i]);
-			}
-		}
-		// Return a message if the term doesn't exist in our DB
-		return res.json({
-			"Response": "False",
-			"Error": "Movie not found!"
-			});
-	}else {
-		res.json({
-			"Response": "False",
-			"Error": "No API key or Query provided."
-			});
-	};
-
-  
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(noteData);
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = Date.now();
+  noteData.push(newNote);
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(noteData), 'utf8');
+  res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const noteId = parseInt(req.params.id);
+  const updatedNoteData = noteData.filter(note => note.id !== noteId);
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(updatedNoteData), 'utf8');
+  res.json(updatedNoteData);
+});
+
+// Start the server
+app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
